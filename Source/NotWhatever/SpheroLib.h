27c75ca1c778e3155338cec146cb4c905a414284
nitzel <jsch12@tu-clausthal.de>
2015-11-25 19:41:36 +0100

    added cpp source and project files

---
+++ Source/NotWhatever/SpheroLib.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SpheroLib.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOTWHATEVER_API USpheroLib : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
