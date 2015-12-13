3fd362978ad6d4e988435eb6cfeb5a814c353283
Thibault Rapin <thibault.rapin@insa-rennes.fr>
2015-12-13 07:15:16 +0100

    Connection to the ball OK

--- Source/NotWhatever/NotWhatever.Build.cs
+++ Source/NotWhatever/NotWhatever.Build.cs
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class NotWhatever : ModuleRules
 {
@@ -9,6 +10,8 @@ public class NotWhatever : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        
+        LoadSpheroLibrary(Target);
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
@@ -23,4 +26,39 @@ public class NotWhatever : ModuleRules
 		//		}
 		// }
 	}
+    
+    private string ModulePath
+    {
+        get { return Path.GetDirectoryName( RulesCompiler.GetModuleFilename( this.GetType().Name ) ); }
+    }
+ 
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath( Path.Combine( ModulePath, "../../ThirdParty/" ) ); }
+    }
+    
+    public bool LoadSpheroLibrary(TargetInfo Target)
+    {
+        bool isLibrarySupported = false;
+ 
+        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+        {
+            isLibrarySupported = true;
+ 
+            //string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
+            string LibrariesPath = Path.Combine(ThirdPartyPath, "SpheroLibrary", "Libraries");
+ 
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "SpheroLibrary.lib")); 
+        }
+ 
+        if (isLibrarySupported)
+        {
+            // Include path
+            PublicIncludePaths.Add( Path.Combine( ThirdPartyPath, "SpheroLibrary", "Includes" ) );
+        }
+ 
+        Definitions.Add(string.Format( "WITH_SPHERO_LIBRARY_BINDING={0}", isLibrarySupported ? 1 : 0 ) );
+ 
+        return isLibrarySupported;
+    }
 }
--- Source/NotWhatever/SpheroLib.cpp
+++ Source/NotWhatever/SpheroLib.cpp
@@ -3,6 +3,49 @@
 #include "NotWhatever.h"
 #include "SpheroLib.h"
 
+#include "stdafx.h"
+#include <windows.h>
+#include "SpheroRAWItf.h"
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+//======================================================================================================================
+using namespace std;
+
+
+void PrintDeviceStatus(string action, ISpheroDevice* device) {
+	cout << "Action: " << action << " Result: ";
+
+	if (device == nullptr) {
+		cout << "Error - Sphero handle is invalid" << endl;
+		return;
+	}
+
+	switch (device->state()) {
+	case SpheroState_None: { cout << "SpheroRAW not initialized" << endl; break; }
+	case SpheroState_Error_BluetoothError: { cout << "Error - Couldn't initialize Bluetooth stack" << endl; break; }
+	case SpheroState_Error_BluetoothUnavailable: { cout << "Error - No valid Bluetooth adapter found" << endl; break; }
+	case SpheroState_Error_NotPaired: { cout << "Error - Specified Sphero not Paired" << endl; break; }
+	case SpheroState_Error_ConnectionFailed: { cout << "Error - Connecting failed" << endl; break; }
+	case SpheroState_Disconnected: { cout << "Sphero disconnected" << endl; break; }
+	case SpheroState_Connected: { cout << "Sphero connected" << endl; break; }
+	}
+
+	cout << endl;
+}
+
+void OrbBasicAppendLine(ISpheroDevice* device, std::string lineText) {
+	std::vector<ubyte> program(lineText.begin(), lineText.end());
+	program.insert(program.end(), '\n');
+	device->appendOrbBasicFragment(0, program);
+}
+
+//======================================================================================================================
+
+ISpheroDevice *device;
 
 // Sets default values for this component's properties
 USpheroLib::USpheroLib()
@@ -12,17 +55,65 @@ USpheroLib::USpheroLib()
 	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 
+	// Create device 
+	device = SpheroRAW_Create("Sphero-GYY");;
 
-	// TODO init sphero ball here
+	while (!(device->state() == SpheroState_Connected)) {
+		// Connect 
+		device->connect();
+		PrintDeviceStatus("Connecting", device);
+		device->abortOrbBasicProgram();
+	}
+
+	if (device->state() == SpheroState_Connected) {
+
+		// Show back light
+		device->setBackLEDOutput(0xff);
+
+		// Set stabilisation off
+		device->setStabilisation(false);
+
+		// Start data stream
+		CommandParameters cp;
+		// N (frequency = 400Hz / N)
+		cp.push_back(0x0);
+		cp.push_back(0xA);
+		// M (number of message at the same time; queuing)
+		cp.push_back(0x0);
+		cp.push_back(0x1);
+		// MASK
+		cp.push_back(0xFF);
+		cp.push_back(0xFF);
+		cp.push_back(0xFF);
+		cp.push_back(0xFF);
+		// PCNT (Packet count; 0 for infinite)
+		cp.push_back(0x0);
+		//MASK2
+		cp.push_back(0xFF);
+		cp.push_back(0xFF);
+		cp.push_back(0xFF);
+		cp.push_back(0xFF);
+
+		device->setDataStreaming(cp);
+	}
+	else {
+		cout << "not connected anymore" << endl;
+	}
 }
 
 
 // destructor
 USpheroLib::~USpheroLib()
 {
-	// TODO release sphero ball here
+	// Disconnect 
+	device->disconnect();
+	PrintDeviceStatus("Disconnect", device);
+
+	//TODO should we remove backlight/datastream and stabilization ?
+
+	// Destroy device 
+	SpheroRAW_Destroy(device); device = nullptr;
 }
 
 
@@ -48,6 +139,43 @@ void USpheroLib::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompo
 
 void USpheroLib::updateData()
 {
+	if (device->state() == SpheroState_Connected) {
+		std::vector<SpheroMessage> messages = device->receive();
+		for (auto message : messages) {
+
+			if (message.responseCode == ResponseCode_OK) {
+				if (message.idCode == AsyncResponseId_SensorDataStreaming) {
+					// Get the angles from dataStream
+
+					for (unsigned short i = 0; i < 3; i++) {
+						// concatenate 2 bytes to make a short
+						unsigned char high = message.data[(i + 13) * 2];
+						unsigned char low = message.data[(i + 13) * 2 + 1];
+						short val = ((high << 8) + low);
+						vecRotation[i] = val;
+						//std::cout << "[" << parameters[i] << ":" << std::dec << val << "]";
+					}
+					std::cout << std::endl;
+
+				}
+				else {
+					std::cout << "[Print] ";
+					unsigned short i = 0;
+					for (i = 0; i < message.data.size() / 2; i++) {
+						// concatenate 2 bytes to make a short
+						unsigned char high = message.data[i * 2];
+						unsigned char low = message.data[i * 2 + 1];
+						short val = ((high << 8) + low);
+						std::cout << std::dec << val << " ";
+					}
+					std::cout << std::endl;
+				}
+			}
+		}
+	}
+
+
+
 	// todo get data from sphero
 	vecRotation += FVector(1, 0, 0);
 }
